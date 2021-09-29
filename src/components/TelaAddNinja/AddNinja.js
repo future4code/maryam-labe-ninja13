ed7c0ae81c9ef4de4150ba43fe394dd1ef58d081
Jessica <bentojessica48@gmail.com>
2021-09-29 11:52:36 -0300

    atualizacoes

--- src/components/TelaAddNinja/AddNinja.js
+++ src/components/TelaAddNinja/AddNinja.js
@@ -2,15 +2,21 @@ import React from "react";
 import styled from "styled-components";
 import axios from "axios";
 
+const ContainerGrid = styled.div`
+    display: inline-grid;
+    grid-template-columns: repeat(2fr) 1fr ;
+    width:100vw;
+    height:100vh;
+`
+
 const ContainerCadastro = styled.div`
     display:inline-flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     margin-left: 50%;
-    height:100vh;
-    width:50%;
     background-color: #7867BF;
+    border: 1px solid black;
 
     input{
         margin-top: 10px;
@@ -35,91 +41,96 @@ const ContainerText = styled.div`
     display:inline-flex;
     flex-direction: column;
     align-items: center;
-    height:100vh;
-    width:50%;
+    margin-right: 50%;
+    border: 1px solid black;
 `
 export default class AddNinja extends React.Component{
     state={
-        titulo: "",
-        descricao: "",
-        preco: "",
-        formaDPagamento: [],
-        data: ''
+
+        title: "",
+        description: "",
+        price: "",
+        paymentMethods: [],
+        dueDate: ''
 
     }
 
     handleTitulo = (e)=>{
-        this.setState({titulo: e.target.value})
+        this.setState({title: e.target.value})
     }
 
     handleDescricao = (e)=>{
-        this.setState({descricao: e.target.value})
+        this.setState({description: e.target.value})
     }
 
     handlePreco = (e)=>{
-        this.setState({preco: e.target.value})
+        this.setState({price: e.target.value})
     }
 
     handleFormaDPagamento = (e)=>{
         let value = Array.from(e.target.selectedOptions, option => option.value)
-        this.setState({formaDPagamento: value})
+        this.setState({paymentMethods: value})
     }
 
     handleData = (e)=>{
-        this.setState({data: e.target.value})
+        this.setState({dueDate: e.target.value})
     }
+
 
     criarEmprego = ( ) =>{
         const body = {
-            titulo: this.state.titulo,
-            descricao: this.state.descricao,
-            preco: Number(this.state.preco),
-            formaDPagamento: this.state.formaDPagamento,
-            data: this.state.data
+            title: this.state.title,
+            description: this.state.description,
+            price: Number(this.state.price),
+            paymentMethods: this.state.paymentMethods,
+            dueDate: this.state.dueDate
         }
-
+    
         const url = "https://labeninjas.herokuapp.com/jobs"
         
-
         axios.post(url, body, { 
             headers:{
-            Authorization: "fd0c113e-8948-4507-8abd-386524fa9ced"}
+            Authorization: "2bdafc4e-63bc-428c-8b6e-154eedb905d2"}
             })
         .then((res)=>{
+            this.setState({title:"", description: "", price: "", paymentMethods: [], dueDate: "",})
+            alert(`Seu serviço ${this.state.title} foi adicionado com sucesso!`)
             console.log(res.data)
         })
         .catch((error)=>{
-            console.log(error)
+            console.log(error.response)
         })
     }
 
     render(){
-
+        
         return(
-            <div>
+            <ContainerGrid>
                 <ContainerText>
                 <h1>SE TORNE UM NINJA</h1>
                 <p>Mostre-nos seu talento!</p>
                 </ContainerText>
                 <ContainerCadastro>
                 <input
+                type="text"
                 placeholder={"Titulo"}
-                value={this.state.titulo}
+                value={this.state.title}
                 onChange={this.handleTitulo}
                 />
                  <input
+                 type="text"
                 placeholder={"Descrição"}
-                value={this.state.descricao}
+                value={this.state.description}
                 onChange={this.handleDescricao}
                 />
                  <input
                  type= "Number"
                 placeholder={"Preço"}
-                value={this.state.preco}
+                value={this.state.price}
                 onChange={this.handlePreco}
                 />
                 
-                 <select multiple value={this.state.formaDPagamento} 
+                 <select multiple value={this.state.paymentMethods} 
                  onChange={this.handleFormaDPagamento}
                  placeholder={"Forma de Pagamento"}>
                      <option>Cartão de Débito</option>
@@ -129,15 +140,14 @@ export default class AddNinja extends React.Component{
                      <option>Pix</option>
                  </select>
                  <input
-                 id="date"
                  type="date"
-                 value={this.state.data}
+                 value={this.state.dueDate}
                  onChange={this.handleData}
                  />
                  <button onClick={this.criarEmprego}>Cadastrar Serviços</button>
                  </ContainerCadastro>
                  
-            </div>
+            </ContainerGrid>
         )
     }
 }
