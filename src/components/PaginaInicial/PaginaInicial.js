984c8eff864b0be45f71cc23d14723187a7f11a3
Laura Laurena <lauralaurena0204@gmail.com>
2021-09-29 16:40:28 -0300

    Atualizações do background

--- src/components/PaginaInicial/PaginaInicial.js
+++ src/components/PaginaInicial/PaginaInicial.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import styled from 'styled-components';
-
-
+import backInicioECards from '../../img/backInicioECards.png'
 
 const DivCentralizada= styled.div`
 display: flex;
@@ -10,34 +9,62 @@ flex-direction: column;
 align-items: center;
 justify-content: center;
 height: 100vh;
-background-color:#836FFF;
+/* background-color:#836FFF; */
+img{
+    position:relative ;
+    width:100vw;
+    height:100vh;
+}
 `
 const Titulo= styled.h1`
 font-size: 130px;
-margin: 0%;
+margin-bottom:15%;
 color:black;
+position: absolute;
 `
 const Slogan= styled.p`
 font-size: 26px;
-margin: 0%;
+margin-bottom: 30%;
 color: black;
+position: absolute;
 `
-const Botoes= styled.button`
+const Botao1= styled.button`
 padding:8px;
 margin: 10px;
 border-radius: 6px;
 background-color:black;
 color:lightgray ;
+position: absolute;
+/* z-index:1; */
+top:60%;
+left:52%;
+display: flex;
+justify-content:space-evenly;
 `
+const Botao2= styled.button`
+padding:8px;
+margin: 10px;
+border-radius: 6px;
+background-color:black;
+color:lightgray ;
+position: absolute;
+/* z-index:1; */
+top:60%;
+right:52%;
+display: flex;
+justify-content:space-evenly;
+`
+
 export default class PaginaInicial extends React.Component{
     render(){
         return(
             <DivCentralizada>
+                <img src={require("../../img/backInicioECards.png")}/>
                 <Slogan>O talento certo no momento certo</Slogan>
                 <Titulo>L A B E N I N J A</Titulo>
                 <div>
-                <Botoes>Se torne um ninja</Botoes>
-                <Botoes>Contrate um ninja</Botoes>
+                <Botao1>Se torne um ninja</Botao1>
+                <Botao2>Contrate um ninja</Botao2>
                 </div>
             </DivCentralizada>
         )
